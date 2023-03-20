dd79db5b088ca803d999295a9745a948404651b1
Callum <callumoneill1999@hotmail.co.uk>
2023-03-20 02:17:40 +0000

    Adding client side filtering of items in dashboard view

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,25 +1,26 @@
 <script setup>
 import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue';
 import { Head } from '@inertiajs/inertia-vue3';
-import { defineProps, ref, watch } from "vue";
+import { defineProps, ref, watchEffect } from "vue";
 import Card from "@/Components/Card";
 import { Inertia } from '@inertiajs/inertia';
+import { data } from 'browserslist';
+import { exportDefaultSpecifier } from '@babel/types';
 
 
 const props = defineProps({
     items: Array,
-    filters: Object
+    filters: Object,
 })
 
 const search = ref(props.filters.search);
 
-watch(search, value => {
-    Inertia.get('/dashboard', {search: value}, {
+watchEffect( () => {
+    Inertia.get('/dashboard', {search: search.value}, {
         preserveState : true,
         replace : true
         }
     );
-
 })
 </script>
 
@@ -40,15 +41,26 @@ watch(search, value => {
                         <h3 class="text-2xl">Search</h3>
                         <div class="search-container">
                                 <input id="search" name="search_query" v-model="search" type="text" placeholder="Search Items..." />
+                                <div class="filter-by">
+                                        <label for="info-filter" class="ml-3"> Info
+                                            <input type="checkbox" name="info-filter" id="info-filter" value="info" v-model="types" />
+                                        </label>
+                                        <label for="download-filter" class="ml-3"> Download
+                                            <input type="checkbox" name="download-filter" id="download-filter" value="download" v-model="types"/>
+                                        </label>
+                                        <label for="weblink-filter" class="ml-3"> Weblink
+                                            <input type="checkbox" name="weblink-filter" id="weblink-filter" value="weblink" v-model="types" />
+                                        </label>
+                                </div>
+                            </div>
                         </div>
-                    </div>
                 </div>
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mt-4">
                     <div class="p-6 bg-white border-b border-gray-200">
                         <h3 class="text-2xl">Latest Items</h3>
 
                         <div class="grid gap-4 grid-cols-6 mt-4">
-                            <Card v-for="item in items" :key="item.id" :item="item" />
+                            <Card v-for="item in getFilteredItems()" :key="item.id" :item="item" />
                         </div>
                     </div>
                 </div>
@@ -78,16 +90,30 @@ watch(search, value => {
   box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
     rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
 }
-#search-button {
-  display: block;
-  width: 350px;
-  margin: 20px auto;
-  padding: 10px 45px;
-  background-size: 15px 15px;
-  font-size: 16px;
-  border: none;
-  border-radius: 5px;
-  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
-    rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+
+[type="checkbox"]
+{
+    vertical-align:middle;
 }
+
 </style>
+<script>
+export default{
+    data(){
+        return{
+           types:["info", "download", "weblink"],
+           items: this.items,
+        };
+    },
+    methods: {
+        getFilteredItems(){
+            let filteredItems = [];
+            this.types.forEach((type) => {
+                let foundItems = this.items.filter(e => e.content_type.toLowerCase() === type);
+                filteredItems = filteredItems.concat(foundItems);
+            })
+            return filteredItems;
+        }
+    }
+};
+</script>
